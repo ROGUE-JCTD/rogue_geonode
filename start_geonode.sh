4593de23060a3cedf03fe64295cba96f4d35ad5e
kzusy <kevin.zusy@lmnsolutions.com>
2013-06-27 10:53:45 -0700

    Start geonode in the background

--- start_geonode.sh
+++ start_geonode.sh
@@ -3,4 +3,4 @@
 # For this to work perform the following command to change permissions
 # sudo chmod 755 -R /var/lib/geonode;
 #sudo -u www-data -g www-data /var/lib/geonode/bin/uwsgi --ini /var/lib/geonode/rogue_geonode/django.ini
-/var/lib/geonode/bin/uwsgi --ini /var/lib/geonode/rogue_geonode/django.ini
+/var/lib/geonode/bin/uwsgi --ini /var/lib/geonode/rogue_geonode/django.ini &
