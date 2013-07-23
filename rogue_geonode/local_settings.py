79639642c126b39311ea4cb5642298b4f0deb662
Syrus Mesdaghi <syrusm@gmail.com>
2013-07-23 08:20:50 -0700

    Updated local_settings.py for automated install...

--- rogue_geonode/local_settings.py
+++ rogue_geonode/local_settings.py
@@ -5,9 +5,11 @@ UPLOADER_BACKEND_URL = 'importer'
 DATABASES = {
     'default': {
          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'geonode',
-         'USER': 'geonode',
-         'PASSWORD': 'geonode',
+         'NAME': '<APPLICATION_DB_NAME>',
+         'USER': '<APPLICATION_USER_NAME>',
+         'PASSWORD': '<APPLICATION_USER_PASSWORD>',
+	 'HOST': '<APPLICATION_DB_HOST>',
+	 'PORT': '<APPLICATION_DB_PORT>',
      }
 }
 
@@ -16,19 +18,20 @@ DATABASES = {
 #Import uploaded shapefiles into a database such as PostGIS?
 DB_DATASTORE = True
 
-UPLOADER_SHOW_TIME_STEP=False
+UPLOADER_SHOW_TIME_STEP = True
 
 #Database datastore connection settings
-DB_DATASTORE_DATABASE = 'geonode_imports'
-DB_DATASTORE_USER = 'geonode'
-DB_DATASTORE_PASSWORD = 'geonode'
-DB_DATASTORE_HOST = 'localhost'
-DB_DATASTORE_PORT = '5432'
-DB_DATASTORE_TYPE = 'postgis'
-DB_DATASTORE_NAME = 'geonode_imports'
+DB_DATASTORE_DATABASE = '<DB_DATASTORE_DATABASE>'
+DB_DATASTORE_USER = '<DB_DATASTORE_USER>'
+DB_DATASTORE_PASSWORD = '<DB_DATASTORE_PASSWORD>'
+DB_DATASTORE_HOST = '<DB_DATASTORE_HOST>'
+DB_DATASTORE_PORT = '<DB_DATASTORE_PORT>'
+DB_DATASTORE_TYPE = '<DB_DATASTORE_TYPE>'
+DB_DATASTORE_NAME = '<DB_DATASTORE_NAME>'
 
 #Database datastore connection settings
-#GEOGIT_DATASTORE_NAME = 'geogit-repo'
+GEOGIT_DATASTORE_NAME = 'geogit-repo'
+GEOGIT_DATASTORE = True
 
 # Use the printNG geoserver lib
 PRINTNG_ENABLED=True
