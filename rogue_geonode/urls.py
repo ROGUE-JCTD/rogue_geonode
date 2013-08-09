898a28e092f55422239ebb6b68930a6c06c3748d
Tyler Garner <garnertb@gmail.com>
2013-08-09 12:00:00 -0400

    Fixed typo in urls.py

--- rogue_geonode/urls.py
+++ rogue_geonode/urls.py
@@ -27,7 +27,7 @@ urlpatterns = patterns('',
 
     # Static pages
     url(r'^$', 'geonode.views.index', {'template': 'site_index.html'}, name='home'),
-    url(r'^help/$', TemplateView.as_view(template_name="helo.html"), name='help'), 
+    url(r'^help/$', TemplateView.as_view(template_name="help.html"), name='help'), 
     url(r'^developer/$', TemplateView.as_view(template_name="developer.html"), name='developer'),
     url(r'^about/$', TemplateView.as_view(template_name="about.html"), name='about'),
 
