4d46a04a00b5406cb761a4839849512a1e725630
Tyler Garner <garnertb@gmail.com>
2014-07-21 13:36:33 -0400

    Remove overridden settings and urls.

--- rogue_geonode/settings.py
+++ rogue_geonode/settings.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Django settings for the rogue_geonode project.
-import os
 import geonode
+import os
 
-#
-# General Django development settings
-#
+from geonode.settings import *
 
 SITENAME = 'rogue_geonode'
 
@@ -17,21 +14,57 @@ GEONODE_ROOT = os.path.abspath(os.path.dirname(geonode.__file__))
 
 # Setting debug to true makes Django serve static media and
 # present pretty error pages.
-DEBUG = TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = ()
+CACHE_TIME = 0
+CLASSIFICATION_BANNER_ENABLED = False
+DEBUG = TEMPLATE_DEBUG = True
+GEOGIT_DATASTORE_NAME = 'DEFAULT_NAME'
+LOCKDOWN_GEONODE = True
+REGISTRATION_OPEN = False
+SOCIAL_BUTTONS = False
 
 # Set to True to load non-minified versions of (static) client dependencies
 DEBUG_STATIC = False
 
 # This is needed for integration tests, they require
 # geonode to be listening for GeoServer auth requests.
-os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = 'localhost:8000'
+
 
 # Defines settings for development
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(PROJECT_ROOT, 'development.db'),
+    }
+}
+
+# OGC (WMS/WFS/WCS) Server Settings
+OGC_SERVER = {
+    'default': {
+        'BACKEND': 'geonode.geoserver',
+        'LOCATION': 'http://localhost:8080/geoserver/',
+        'PUBLIC_LOCATION': 'http://localhost:8080/geoserver/',
+        'USER': 'admin',
+        'PASSWORD': 'geoserver',
+        'MAPFISH_PRINT_ENABLED': True,
+        'PRINTNG_ENABLED': True,
+        'GEONODE_SECURITY_ENABLED': True,
+        'GEOGIT_ENABLED': True,
+        'WMST_ENABLED': False,
+        'BACKEND_WRITE_ENABLED': True,
+        'WPS_ENABLED': False,
+        # Set to name of database in DATABASES dictionary to enable
+        'DATASTORE': '',
+        'TIMEOUT': 10,  # The maximum time to wait for the server to respond.
+    }
+}
+
+# Uploader Settings
+UPLOADER = {
+    'BACKEND': 'geonode.importer',
+    'OPTIONS': {
+        'TIME_ENABLED': False,
+        'GEOGIT_ENABLED': True,
     }
 }
 
@@ -46,57 +79,13 @@ TIME_ZONE = 'America/Chicago'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en'
 
-LANGUAGES = (
-    ('en', 'English'),
-    ('es', 'Español'),
-    ('it', 'Italiano'),
-    ('fr', 'Français'),
-    ('de', 'Deutsch'),
-    ('el', 'Ελληνικά'),
-    ('id', 'Bahasa Indonesia'),
-    ('zh-cn', '中文'),
-    ('ja', '日本語'),
-    ('fa', 'Persian'),
-    ('pt', 'Portuguese'),
-    ('ru', 'Russian'),
-    ('vi', 'Vietnamese'),
-)
-
 WSGI_APPLICATION = "rogue_geonode.wsgi.application"
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "uploaded")
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = "/uploaded/"
-
-# Absolute path to the directory that holds static files like app media.
-# Example: "/home/media/media.lawrence.com/apps/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "static_root")
-
-# URL that handles the static files like app media.
-# Example: "http://media.lawrence.com"
-STATIC_URL = "/static/"
 
 # Additional directories which hold static files
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, "static"),
     os.path.join(GEONODE_ROOT, "static"),
 ]
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
 
 # Note that Django automatically includes the "templates" dir in all the
 # INSTALLED_APPS, se there is no need to add maps/templates or admin/templates
@@ -117,91 +106,22 @@ SECRET_KEY = 'x-#u&4x2k*$0-60fywnm5&^+&a!pd-ajrx(z@twth%i7^+oskh'
 # Location of url mappings
 ROOT_URLCONF = 'rogue_geonode.urls'
 
-# Site id in the Django sites framework
-SITE_ID = 1
-
-# Login and logout urls override
-LOGIN_URL = '/account/login/'
-LOGOUT_URL = '/account/logout/'
-
-# Activate the Documents application
-DOCUMENTS_APP = True
-
-ALLOWED_DOCUMENT_TYPES = [
-    'doc', 'docx', 'gif', 'jpg', 'jpeg', 'ods', 'odt', 'pdf', 'png', 'ppt',
-    'rar', 'tif', 'tiff', 'txt', 'xls', 'xlsx', 'xml', 'zip',
-]
-
 MAX_DOCUMENT_SIZE = 2  # MB
 
-ROGUE_GEONODE_APPS = (
-    # GeoNode internal apps
-    'geonode.people',
-    'geonode.base',
-    'geonode.layers',
-    'geonode.upload',
-    'geonode.maps',
-    'geonode.proxy',
-    'geonode.security',
-    'geonode.search',
-    'geonode.social',
-    'geonode.catalogue',
-    'geonode.documents',
-    'geonode.geoserver',
-
-    # GeoNode Contrib Apps
-    'geonode.contrib.groups',
-    'rogue_geonode.file_service',
-    'rogue_geonode.core',
-    'maploom',
-)
 
 INSTALLED_APPS = (
-    # Apps bundled with Django
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.admin',
-    'django.contrib.sitemaps',
-    'django.contrib.staticfiles',
-    'django.contrib.messages',
-    'django.contrib.humanize',
-
-    # Third party apps
+    'rogue_geonode.file_service',
+    'rogue_geonode.core',
     'django_classification_banner',
-
-    # Utility
-    'pagination',
-    'taggit',
-    'taggit_templatetags',
-    'south',
-    'friendlytagloader',
-    'geoexplorer',
-    'django_extensions',
-
-    # Theme
-    "pinax_theme_bootstrap_account",
-    "pinax_theme_bootstrap",
-    'django_forms_bootstrap',
-
-    # Social
-    'account',
-    'avatar',
-    'dialogos',
-    'agon_ratings',
-    'notification',
-    'announcements',
-    'actstream',
-    'user_messages',
-) + ROGUE_GEONODE_APPS
+    'maploom',
+) + INSTALLED_APPS
 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
     'formatters': {
         'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+            'format': '%(levelname)s %(name)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
         },
         'simple': {
             'format': '%(message)s'},
@@ -238,7 +158,7 @@ LOGGING = {
         },
         "geonode": {
             "handlers": ["console"],
-            "level": "ERROR",
+            "level": "DEBUG",
         },
 
         "gsconfig.catalog": {
@@ -260,186 +180,10 @@ LOGGING = {
     },
 }
 
-#
-# Customizations to built in Django settings required by GeoNode
-#
-
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    "django.core.context_processors.tz",
-    'django.core.context_processors.media',
-    "django.core.context_processors.static",
-    'django.core.context_processors.request',
-    'django.contrib.messages.context_processors.messages',
-    'account.context_processors.account',
-    'geonode.context_processors.resource_urls',
-    'geonode.geoserver.context_processors.geoserver_urls',
+TEMPLATE_CONTEXT_PROCESSORS += (
     'django_classification_banner.context_processors.classification',
     'rogue_geonode.core.context_processors.security_warnings'
 )
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    # The setting below makes it possible to serve different languages per
-    # user depending on things like headers in HTTP requests.
-    'django.middleware.locale.LocaleMiddleware',
-    'pagination.middleware.PaginationMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # This middleware allows to print private layers for the users that have
-    # the permissions to view them.
-    # It sets temporary the involved layers as public before restoring the permissions.
-    # Beware that for few seconds the involved layers are public there could be risks.
-    # 'geonode.middleware.PrintProxyMiddleware',
-)
-
-
-# Replacement of default authentication backend in order to support
-# permissions per object.
-AUTHENTICATION_BACKENDS = ('geonode.security.auth.GranularBackend',)
-
-
-def get_user_url(u):
-    return u.profile.get_absolute_url()
-
-
-ABSOLUTE_URL_OVERRIDES = {
-    'auth.user': get_user_url
-}
-
-#
-# Settings for default search size
-#
-DEFAULT_SEARCH_SIZE = 10
-
-CLASSIFICATION_BANNER_ENABLED = False
-
-#
-# Settings for third party apps
-#
-
-# Agon Ratings
-AGON_RATINGS_CATEGORY_CHOICES = {
-    "maps.Map": {
-        "map": "How good is this map?"
-    },
-    "layers.Layer": {
-        "layer": "How good is this layer?"
-    },
-    "documents.Document": {
-        "document": "How good is this document?"
-    }
-}
-
-# Activity Stream
-ACTSTREAM_SETTINGS = {
-    'MODELS': ('auth.user', 'layers.layer', 'maps.map', 'dialogos.comment', 'documents.document'),
-    'FETCH_RELATIONS': True,
-    'USE_PREFETCH': True,
-    'USE_JSONFIELD': True,
-    'GFK_FETCH_DEPTH': 1,
-}
-
-# For South migrations
-SOUTH_MIGRATION_MODULES = {
-    'avatar': 'geonode.migrations.avatar',
-}
-SOUTH_TESTS_MIGRATE = False
-
-# Settings for Social Apps
-AUTH_PROFILE_MODULE = 'people.Profile'
-REGISTRATION_OPEN = False
-
-# Email for users to contact admins.
-THEME_ACCOUNT_CONTACT_EMAIL = 'admin@example.com'
-
-#
-# Test Settings
-#
-
-# Setting a custom test runner to avoid running the tests for
-# some problematic 3rd party apps
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-# Arguments for the test runner
-NOSE_ARGS = [
-    '--nocapture',
-    '--detailed-errors',
-]
-
-
-# GeoNode specific settings
-#
-SITEURL = "http://localhost:8000/"
-
-# GeoNode javascript client configuration
-
-# Where should newly created maps be focused?
-DEFAULT_MAP_CENTER = (0, 0)
-
-# How tightly zoomed should newly created maps be?
-# 0 = entire world;
-# maximum zoom is between 12 and 15 (for Google Maps, coverage varies by area)
-DEFAULT_MAP_ZOOM = 0
-
-MODIFY_TOPICCATEGORY = False
-
-MISSING_THUMBNAIL = 'geonode/img/missing_thumb.png'
-
-# Notify the user via email when their password is changed.
-# Disabled by default since this view will throw a 500 if no mail server is configured
-ACCOUNT_NOTIFY_ON_PASSWORD_CHANGE = False
-
-# Require the user to confirm their email
-# Disabled by default, requires a mail server to be configured
-ACCOUNT_EMAIL_CONFIRMATION_REQUIRED = False
-
-CACHE_TIME = 0
-
-META_DOWNLOAD_ALLOWS = True
-
-# OGC (WMS/WFS/WCS) Server Settings
-OGC_SERVER = {
-    'default': {
-        'BACKEND': 'geonode.geoserver',
-        'LOCATION': 'http://localhost:8080/geoserver/',
-        'PUBLIC_LOCATION': 'http://localhost:8080/geoserver/',
-        'USER': 'admin',
-        'PASSWORD': 'geoserver',
-        'MAPFISH_PRINT_ENABLED': True,
-        'PRINTNG_ENABLED': True,
-        'GEONODE_SECURITY_ENABLED': True,
-        'GEOGIT_ENABLED': False,
-        'WMST_ENABLED': False,
-        'BACKEND_WRITE_ENABLED': True,
-        'WPS_ENABLED': False,
-        # Set to name of database in DATABASES dictionary to enable
-        'DATASTORE': '',
-        'TIMEOUT': 10,  # The maximum time to wait for the server to respond.
-    }
-}
-
-# Uploader Settings
-UPLOADER = {
-    'BACKEND': 'geonode.rest',
-    'OPTIONS': {
-        'TIME_ENABLED': False,
-        'GEOGIT_ENABLED': False,
-    }
-}
-
-
-METADATA_DOWNLOAD_ALLOWS = True
-
-SOCIAL_BUTTONS = False
-
-# Require users to authenticate before using Geonode
-LOCKDOWN_GEONODE = True
 
 # Add additional paths (as regular expressions) that don't require authentication.
 AUTH_EXEMPT_URLS = ('/file-service/*', '/i18n/setlang/',)
@@ -447,105 +191,6 @@ AUTH_EXEMPT_URLS = ('/file-service/*', '/i18n/setlang/',)
 if LOCKDOWN_GEONODE:
     MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + ('geonode.security.middleware.LoginRequiredMiddleware',)
 
-
-DOWNLOAD_FORMATS_METADATA = [
-    'Atom', 'DIF', 'Dublin Core', 'ebRIM', 'FGDC', 'TC211',
-]
-DOWNLOAD_FORMATS_VECTOR = [
-    'JPEG', 'PDF', 'PNG', 'Zipped Shapefile', 'GML 2.0', 'GML 3.1.1', 'CSV',
-    'Excel', 'GeoJSON', 'KML', 'View in Google Earth', 'Tiles',
-]
-
-DOWNLOAD_FORMATS_RASTER = [
-    'JPEG', 'PDF', 'PNG', 'ArcGrid', 'GeoTIFF', 'Gtopo30', 'ImageMosaic', 'KML',
-    'View in Google Earth', 'Tiles',
-]
-
-HAYSTACK_SEARCH = False
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'geonode',
-        },
-    }
-HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
-HAYSTACK_SEARCH_RESULTS_PER_PAGE = 20
-
-# Load more settings from a file called local_settings.py if it exists
-try:
-    from local_settings import *  # noqa
-except ImportError:
-    pass
-
-# CSW settings
-CATALOGUE = {
-    'default': {
-        # The underlying CSW implementation
-        # default is pycsw in local mode (tied directly to GeoNode Django DB)
-        'ENGINE': 'geonode.catalogue.backends.pycsw_local',
-        # pycsw in non-local mode
-        # 'ENGINE': 'geonode.catalogue.backends.pycsw_http',
-        # GeoNetwork opensource
-        # 'ENGINE': 'geonode.catalogue.backends.geonetwork',
-        # deegree and others
-        # 'ENGINE': 'geonode.catalogue.backends.generic',
-
-        # The FULLY QUALIFIED base url to the CSW instance for this GeoNode
-        'URL': '%scatalogue/csw' % SITEURL,
-        # 'URL': 'http://localhost:8080/geonetwork/srv/en/csw',
-        # 'URL': 'http://localhost:8080/deegree-csw-demo-3.0.4/services',
-
-        # login credentials (for GeoNetwork)
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-    }
-}
-
-# pycsw settings
-PYCSW = {
-    # pycsw configuration
-    'CONFIGURATION': {
-        'metadata:main': {
-            'identification_title': 'GeoNode Catalogue',
-            'identification_abstract': 'GeoNode is an open source platform that facilitates the creation, \
-             sharing, and collaborative use of geospatial data',
-            'identification_keywords': 'sdi,catalogue,discovery,metadata,GeoNode',
-            'identification_keywords_type': 'theme',
-            'identification_fees': 'None',
-            'identification_accessconstraints': 'None',
-            'provider_name': 'Organization Name',
-            'provider_url': SITEURL,
-            'contact_name': 'Lastname, Firstname',
-            'contact_position': 'Position Title',
-            'contact_address': 'Mailing Address',
-            'contact_city': 'City',
-            'contact_stateorprovince': 'Administrative Area',
-            'contact_postalcode': 'Zip or Postal Code',
-            'contact_country': 'Country',
-            'contact_phone': '+xx-xxx-xxx-xxxx',
-            'contact_fax': '+xx-xxx-xxx-xxxx',
-            'contact_email': 'Email Address',
-            'contact_url': 'Contact URL',
-            'contact_hours': 'Hours of Service',
-            'contact_instructions': 'During hours of service. Off on weekends.',
-            'contact_role': 'pointOfContact',
-        },
-        'metadata:inspire': {
-            'enabled': 'true',
-            'languages_supported': 'eng,gre',
-            'default_language': 'eng',
-            'date': 'YYYY-MM-DD',
-            'gemet_keywords': 'Utility and governmental services',
-            'conformity_service': 'notEvaluated',
-            'contact_name': 'Organization Name',
-            'contact_email': 'Email Address',
-            'temp_extent': 'YYYY-MM-DD/YYYY-MM-DD',
-        }
-    }
-}
-
-# GeoNode javascript client configuration
 
 MAP_BASELAYERS = [
     {
@@ -567,3 +212,10 @@ MAP_BASELAYERS = [
         "group":"background"
     }
 ]
+
+
+# Load more settings from a file called local_settings.py if it exists
+try:
+    from local_settings import *  # noqa
+except ImportError:
+    pass
--- rogue_geonode/urls.py
+++ rogue_geonode/urls.py
@@ -1,90 +1,11 @@
-from django.conf.urls import include, patterns, url
-from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf.urls.static import static
-from django.views.generic import TemplateView
-from geonode.sitemap import LayerSitemap, MapSitemap
-import geonode.proxy.urls
-
-# Setup Django Admin
-from django.contrib import admin
-admin.autodiscover()
-
-js_info_dict = {
-    'domain': 'djangojs',
-    'packages': ('geonode',)
-}
-
-sitemaps = {
-    "layer": LayerSitemap,
-    "map": MapSitemap
-}
+from django.conf.urls import url, patterns, include
+from geonode.urls import urlpatterns as geonode_url_patterns
+from maploom.geonode.urls import urlpatterns as maploom_urls
 
 urlpatterns = patterns('',
-                       url(r'^$', 'geonode.views.index', {'template': 'site_index.html'}, name='home'),
-                       url(r'^help/$', TemplateView.as_view(template_name="help.html"), name='help'),
-                       url(r'^developer/$', TemplateView.as_view(template_name="developer.html"), name='developer'),
-                       url(r'^about/$', TemplateView.as_view(template_name="about.html"), name='about'),
-                       (r'^layers/', include('geonode.layers.urls')),
-
-                       # Map views
-                       (r'^maps/', include('geonode.maps.urls')),
-
-                       # MapLoom Urls
-                       url(r'^maploom/maps/new', 'geonode.maps.views.new_map', {'template': 'maps/maploom.html'},
-                           name='maploom-map-new'),
-
-                       url(r'^maploom/maps/(?P<mapid>\d+)/view', 'geonode.maps.views.map_view',
-                           {'template': 'maps/maploom.html'}, name='maploom-map-view'),
-
-                       # Catalogue views
-                       (r'^catalogue/', include('geonode.catalogue.urls')),
-
-                       # Search views
-                       (r'^search/', include('geonode.search.urls')),
-
-                       # Upload views
-                       (r'^upload/', include('geonode.upload.urls')),
-
-                       # GeoServer Helper Views
-                       (r'^gs/', include('geonode.geoserver.urls')),
-
-                       # Social views
-                       (r"^account/", include("account.urls")),
-                       (r'^people/', include('geonode.people.urls')),
-                       (r'^avatar/', include('avatar.urls')),
-                       (r'^comments/', include('dialogos.urls')),
-                       (r'^ratings/', include('agon_ratings.urls')),
-                       (r'^activity/', include('actstream.urls')),
-                       (r'^announcements/', include('announcements.urls')),
-                       # (r'^notifications/', include('notification.urls')),
-                       (r'^messages/', include('user_messages.urls')),
-                       (r'^social/', include('geonode.social.urls')),
-
-                       # Accounts
-                       url(r'^account/ajax_login$', 'geonode.views.ajax_login', name='account_ajax_login'),
-                       url(r'^account/ajax_lookup$', 'geonode.views.ajax_lookup', name='account_ajax_lookup'),
-                       url(r'^security/permissions/(?P<type>[^/]*)/(?P<resource_id>\d+)$',
-                           'geonode.security.views.resource_permissions', name='resource_permissions'),
-
-                       # Meta
-                       url(r'^lang\.js$', TemplateView.as_view(template_name="lang.js"), name='lang'),
-                       url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict, name='jscat'),
-                       url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
-                           {'sitemaps': sitemaps}, name='sitemap'),
-
-                       (r'^i18n/', include('django.conf.urls.i18n')),
-                       (r'^admin/', include(admin.site.urls)),
                        (r'^file-service/', include('rogue_geonode.file_service.urls')),
                        (r'^proxy/', 'rogue_geonode.views.proxy'),
-                       (r'^groups/', include('geonode.contrib.groups.urls')),)
+                       )
 
-# Documents views
-if 'geonode.documents' in settings.INSTALLED_APPS:
-    urlpatterns += patterns('', (r'^documents/', include('geonode.documents.urls')),)
-
-urlpatterns += geonode.proxy.urls.urlpatterns
-
-# Serve static files
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += geonode_url_patterns
+urlpatterns += maploom_urls
