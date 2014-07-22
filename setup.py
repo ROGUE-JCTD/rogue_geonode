cf774b126b9d8853ab9120867e82ec0ba9f4097c
Tyler Garner <garnertb@gmail.com>
2014-07-22 10:27:23 -0400

    Bump the GeoNode commit.

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
         'tests': install_requires + tests_requires,
         'docs':  docs_requires
     },
-    dependency_links=['https://github.com/geonode/geonode/archive/d03e0783d221c0c0e69ee0553c785343f68f9247.tar.gz'
+    dependency_links=['https://github.com/geonode/geonode/archive/49b6a7f343a0c0160b9a2fcd652c6bedc3f9b757.tar.gz'
                       '#egg=geonode-2.0.1a1',
                       'https://github.com/ROGUE-JCTD/django-maploom/archive/master.tar.gz#egg=django-maploom-0.0.1'],
     zip_safe=False,
