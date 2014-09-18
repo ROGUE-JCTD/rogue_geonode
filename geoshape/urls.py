03e29b534f12fbe4155de11f5e49c52e46702207
Tyler Garner <garnertb@gmail.com>
2014-09-18 14:22:28 -0400

    Update the splash page.

--- geoshape/urls.py
+++ geoshape/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import patterns, include
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from geonode.urls import urlpatterns as geonode_url_patterns
 from maploom.geonode.urls import urlpatterns as maploom_urls
 
 urlpatterns = patterns('',
+                       url(r'^$', 'geonode.views.index', dict(template='site_index.html'),  name='home'),
                        (r'^file-service/', include('geoshape.file_service.urls')),
                        (r'^proxy/', 'geoshape.views.proxy'),
                        )
