b0d1e6727e7a5d2cad5db595bbed8ec49f643ef9
John Wileczek <john.wileczek@gmail.com>
2015-02-13 11:02:03 -0500

    Added basic response Capability information to webpage. Data currently not sent to DB.

--- geoshape/firestation/static/firestation/firestation.js
+++ geoshape/firestation/static/firestation/firestation.js
@@ -11,7 +11,17 @@
   })
 
   .controller('fireStationController', function($scope, $window) {
-    $scope.forms = [{'apparatus':'Engine'}];
+    $scope.forms = [{'name':'Add New Response Form',
+					 'apparatus':'Engine',
+					 'firefighter':0,
+					 'firefighter_emt':0,
+					 'firefighter_paramedic':0,
+					 'ems_emt':0,
+					 'ems_paramedic':0,
+					 'officer':0,
+					 'officer_paramedic':0,
+					 'ems_supervisor':0,
+					 'chief':0}];
     var options = {
         boxZoom: false,
         zoom: 15,
@@ -22,7 +32,31 @@
         doubleClickZoom: false,
         fullscreenControl: false
     };
+	$scope.selectedForm = $scope.forms[0];
+	$scope.choices = 		['Engine',
+								'Ladder/Truck/Aerial',
+								'Quint',
+								'Ambulance/ALS',
+								'Ambulance/BLS',
+								'Heavy',
+								'Rescue',
+								'Boat',
+								'Hazmat',
+								'Chief',
+								'Other'];
+	
+	
     $scope.map = L.map('map', options).setView(config.centroid, 13);
+	
+	$scope.UpdateOrAddNewRecord = function(selectedForm) {
+		if(selectedForm.name == 'Add New Response Form') {
+			$scope.action = 'Insert';
+		}
+		else {
+			$scope.action = 'Update';
+		}
+		
+	}
     L.marker(config.centroid).addTo($scope.map);
     L.tileLayer('https://{s}.tiles.mapbox.com/v3/examples.map-i87786ca/{z}/{x}/{y}.png', {'attribution': '© Mapbox'})
         .addTo($scope.map);
