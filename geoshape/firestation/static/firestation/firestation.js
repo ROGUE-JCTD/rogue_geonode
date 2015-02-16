ed1a0993c0f78f5a00ca04421404ddc07b2d11c7
John Wileczek <john.wileczek@gmail.com>
2015-02-15 20:38:45 -0500

    Updated page to display forms in a table rather than selecting a form.

--- geoshape/firestation/static/firestation/firestation.js
+++ geoshape/firestation/static/firestation/firestation.js
@@ -24,6 +24,7 @@
         doubleClickZoom: false,
         fullscreenControl: false
     };
+	
 	$scope.choices = 		['Engine',
 								'Ladder/Truck/Aerial',
 								'Quint',
@@ -35,36 +36,15 @@
 								'Hazmat',
 								'Chief',
 								'Other'];
-    $scope.forms = [{'apparatus':'Engine',
-					 'chief_officer':0,
-					 'ems_emt':0,
-					 'ems_paramedic':0,
-					 'ems_supervisor':0,
-					 'firefighter':0,
-					 'firefighter_emt':0,
-					 'firefighter_paramedic':0,
-					 'firestation':thisFirestation,
-					 'officer':0,
-					 'officer_paramedic':0,
-					 'name':'Add New Response Form'
-					 }];
+    $scope.forms = [];
 					 
-	var getUrl = '/api/v1/capabilities/?format=json';
-	var fireUrl = thisFirestation + '?format=json';
-	var postUrl = getUrl;
-	
-	$scope.firestation = $http.get(fireUrl);
+	var getUrl = '/api/v1/capabilities/?firestation='+config.id+'&format=json';
 	$http.get(getUrl).success(function(data){
 		for(var iForm = 0; iForm < data.meta.total_count; iForm++) {
-			if(data.objects[iForm].firestation == thisFirestation) {
-				
-				data.objects[iForm].name = data.objects[iForm].apparatus + data.objects[iForm].id;
-				$scope.forms.push(data.objects[iForm]);
-			}
+			data.objects[iForm].name = data.objects[iForm].apparatus + data.objects[iForm].id;
 		}
+		$scope.forms = data.objects;
 	});
-	
-	$scope.selectedForm = $scope.forms[0];
 	
     $scope.map = L.map('map', options).setView(config.centroid, 13);
 	
@@ -81,43 +61,42 @@
 			form.chief = 0;
 	}
 	
-	$scope.UpdateOrAddNewRecord = function(selectedForm) {
-		if(selectedForm.name == 'Add New Response Form') {
-			$scope.action = 'Insert';
-		}
-		else {
-			$scope.action = 'Update';
-		}
-		
-		if($scope.action == 'Insert') {
-			$scope.resourceCounters[selectedForm.apparatus]++;
-		
-			$http.post(postUrl,selectedForm).success(function(data,status,headers){
+	$scope.AddForm = function() {
+		var newForm = {'apparatus':'Engine',
+						'chief_officer':0,
+						'ems_emt':0,
+						'ems_paramedic':0,
+						'ems_supervisor':0,
+						'firefighter':0,
+						'firefighter_emt':0,
+						'firefighter_paramedic':0,
+						'firestation':thisFirestation,
+						'officer':0,
+						'officer_paramedic':0
+						};
+		var postUrl = '/api/v1/capabilities/?format=json';
+		$http.post(postUrl,newForm).success(function(data,status,headers){
 				$http.get(headers('Location')+'?format=json').success(function(data){
 				data.name = data.apparatus + data.id;
 				$scope.forms.push(data);
-				$scope.ClearForm($scope.selectedForm);
-				$scope.selectedForm = data;
-			});
-			
-			});	
-		}
-		else {
-			var updateUrl = '/api/v1/capabilities/'+selectedForm.id+'/?format=json';
-			
-			$http.put(updateUrl,selectedForm).success(function(data){
-				selectedForm.name = selectedForm.apparatus + selectedForm.id;
-			});
-			
-			
-		}
+				$scope.ClearForm(data);
+				});
+	
+		});
 	}
 	
-	$scope.DeleteForm = function(selectedForm) {
+	$scope.UpdateForm = function(form) {
+			var updateUrl = '/api/v1/capabilities/'+form.id+'/?format=json';
+			
+			$http.put(updateUrl,form).success(function(data){
+				form.name = form.apparatus + form.id;
+			});
+	}
+	
+	$scope.DeleteForm = function(form) {
 		
-		$http.delete('/api/v1/capabilities/'+selectedForm.id+'/?format=json').success(function(data){
-			$scope.forms.splice($scope.forms.indexOf(selectedForm),1);
-			$scope.selectedForm = $scope.forms[0];
+		$http.delete('/api/v1/capabilities/'+form.id+'/?format=json').success(function(data){
+			$scope.forms.splice($scope.forms.indexOf(form),1);
 		});
 	}
    
