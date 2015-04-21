dcc992a9a2e67f9f7bb0e50a4675f9b22a7f4f24
state-hiu <HIU_INFO@state.gov>
2015-04-20 23:06:10 -0400

    fix

--- geoshape/urls.py
+++ geoshape/urls.py
@@ -6,7 +6,7 @@ from maploom.geonode.urls import urlpatterns as maploom_urls
 urlpatterns = patterns('',
                        (r'^file-service/', include('geoshape.file_service.urls')),
                        (r'^proxy/', 'geoshape.views.proxy'),
-                       url(r'^security/', TemplateView.as_view(template_name='security.html'), name='security'),
+                       url(r'^security/$', TemplateView.as_view(template_name='security.html'), name='security'),
                        url(r'^about/api/', TemplateView.as_view(template_name='api.html'), name='about_api'),
                        url(r'^robots.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name='robots_txt'),
                        ) 
