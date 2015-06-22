e5a308b23b1f5ebbcd6fd839fadcffbed9f3d8a2
Syrus Mesdaghi <syrusm@gmail.com>
2015-06-22 13:14:13 -0400

    added api url

--- geoshape/urls.py
+++ geoshape/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import patterns, include
 from geonode.urls import urlpatterns as geonode_url_patterns
 from maploom.geonode.urls import urlpatterns as maploom_urls
+from tilebundler.api import TilesetResource
 
-urlpatterns = patterns('',
-                       (r'^file-service/', include('geoshape.file_service.urls')),
-                       (r'^tileset/', include('tilebundler.urls', namespace='tilesets')),
-                       (r'^proxy/', 'geoshape.views.proxy'),
-                       )
+tileset_resource = TilesetResource()
+
+urlpatterns = patterns(
+    '',
+    (r'^file-service/', include('geoshape.file_service.urls')),
+    (r'^tileset/', include('tilebundler.urls', namespace='tilesets')),
+    (r'^api/', include(tileset_resource.urls)),
+    (r'^proxy/', 'geoshape.views.proxy')
+)
 
 urlpatterns += geonode_url_patterns
 urlpatterns += maploom_urls
