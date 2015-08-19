5fee293577684940658eead8243e3fa81e6f3eb5
Syrus Mesdaghi <syrusm@gmail.com>
2015-08-19 15:55:08 -0400

    bumped maploom version

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ for dirpath, dirnames, filenames in os.walk(walk_dir):
 install_requires = [
         "geoshape-geonode==1.2.1",
         "django-classification-banner>=0.1.4",
-        "django-maploom==1.2.1",
+        "django-maploom==1.2.2",
         "psycopg2==2.4.5",
         "django-tilebundler==0.1-beta1",
         "django-gsschema==0.1-beta1",
