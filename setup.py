2d1dd58e1c1e6dba88fca508bbce08985aa1a038
Syrus Mesdaghi <syrusm@gmail.com>
2015-08-20 14:53:28 -0400

    using newer geonode

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ for dirpath, dirnames, filenames in os.walk(walk_dir):
         data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
 install_requires = [
-        "geoshape-geonode==1.2.1",
+        "geoshape-geonode==1.3",
         "django-classification-banner>=0.1.4",
         "django-maploom==1.2.2",
         "psycopg2==2.4.5",
